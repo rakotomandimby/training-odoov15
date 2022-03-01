2b8468fd29e2a690b43416dbaee24ee341e89a2b
Mihamina <mihamina@ibonia.mg>
2022-03-01 09:58:48 +0300

    Add the Visit object

---
+++ kasia_training/models/visit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Visit(models.Model):
+    _name = 'kasia_training.visit'
+    _description = 'Ship owner of the Kasia training module'
+    type = fields.Selection([
+        ('random', 'Random'),
+        ('planned', 'Planned'),
+        ('oncomplaint', 'On complaint')])
+    occurDate = fields.Date()
+    endValidityDate = fields.Date()
+    about=fields.Html(string='About')
